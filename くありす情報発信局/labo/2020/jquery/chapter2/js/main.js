02946a31a052898642e9cddd58c03acadce96dbe
kuarisu01 <54207095+kuarisu01@users.noreply.github.com>
2020-06-05 01:30:49 +0900

    途中

--- くありす情報発信局/labo/2020/jquery/chapter2/js/main.js
+++ くありす情報発信局/labo/2020/jquery/chapter2/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
     // $('#typo').css('color', '#ebc000');
     // $('#typo .inner').css('transform', 'rotate(10deg)');
-    $('.page-main > div:nth-child(1) .inner').css('opacity', 0.5);
+    // $('.page-main > div:nth-child(1) .inner').css('opacity', 0.5);
+    $('#typo').on('mouseover', function(){
+        $('#typo').css('color', '#ebc000')
+    })
+
 });

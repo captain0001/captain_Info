d1c52f8dad87559b883e8f19387f72fa3d984a07
kuarisu01 <54207095+kuarisu01@users.noreply.github.com>
2020-06-08 11:51:34 +0900

    前回までの更新内容

--- くありす情報発信局/labo/2020/jquery/chapter2/js/main.js
+++ くありす情報発信局/labo/2020/jquery/chapter2/js/main.js
@@ -1,9 +1,32 @@
+// $(function(){
+//     // $('#typo').css('color', '#ebc000');
+//     // $('#typo .inner').css('transform', 'rotate(10deg)');
+//     // $('.page-main > div:nth-child(1) .inner').css('opacity', 0.5);
+//     $('#typo').on('click', function(){
+//         $('#typo').css('background-color', '#ae5e9b')
+//         $('header').css('color', '#ae5e9b')
+//     })
+// });
+// $(function(){
+//     $('#typo').on('mouseover', function(){
+//         $('#typo').css({
+//             color: '#ebc000',
+//             backgroundColor: '#ae5e9b'
+//         })
+//     })
+//     $('#typo').on('mouseout', function(){
+//         $('#typo').css({
+//             color: '',
+//             backgroundColor: ''
+//         })
+//     })
+// });
 $(function(){
-    // $('#typo').css('color', '#ebc000');
-    // $('#typo .inner').css('transform', 'rotate(10deg)');
-    // $('.page-main > div:nth-child(1) .inner').css('opacity', 0.5);
-    $('#typo').on('mouseover', function(){
-        $('#typo').css('color', '#ebc000')
-    })
-
+    $('#typo').on('click', function(){
+        $('#typo .inner').animate({
+            color: '#ebc000'
+        },
+        1500
+        );
+    });
 });
